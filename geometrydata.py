a2ecf5517b72d740f8a59b14255e743100f03324
Harsh Parmar <37501628+Dark-Fyre@users.noreply.github.com>
2019-03-04 08:24:48 +0530

    File addition and minor improvements
    Previously the user had to run three files to obtain the html, chopped it down to and added some important missing files

--- geometrydata.py
+++ geometrydata.py
@@ -8,38 +8,39 @@ def itfile(filepath):
     fp=csv.reader(file)
     return fp
 
-index=0
-file=open('world1.json','w+')
-i=[]
-country=[]
-geometry=json.load(open('countries.json'))
-population=itfile('populationdata.csv')
-index=0
+def geodata():
+        index=0
+        file=open('world.json','w+')
+        i=[]
+        country=[]
+        geometry=json.load(open('countries.json'))
+        population=itfile('populationdata.csv')
+        index=0
 
-for row in population:
-    if (row[1]!='Country'):
-        country.append(row[1])
-for j in range(3):
-	index=0
-	for i in geometry['features']:
-			if (i['properties']['ADMIN'] in country):
-				 pass
-			else:
-				 del geometry['features'][index]
-			index+=1
-index=0
+        for row in population:
+                if (row[1]!='Country'):
+                        country.append(row[1])
+        for j in range(3):
+                index=0
+                for i in geometry['features']:
+                                if (i['properties']['ADMIN'] in country):
+                                        pass
+                                else:
+                                        del geometry['features'][index]
+                                index+=1
+        index=0
 
-for i in geometry['features']:
-    population=itfile('populationdata.csv')
-    count=0
-    for column in population:
-        if (i['properties']['ADMIN']==column[1]):
-            i['properties']['POP2018']=column[2]
-            count=1
-            int(i['properties']['POP2018'])
-    if(count==0):
-        pass
-        #del geometry['features'][index]
-    
-json.dump(geometry,file)
-file.close()
+        for i in geometry['features']:
+                population=itfile('populationdata.csv')
+                count=0
+                for column in population:
+                        if (i['properties']['ADMIN']==column[1]):
+                                i['properties']['POP2018']=column[2]
+                                count=1
+                                int(i['properties']['POP2018'])
+        if(count==0):
+                pass
+                #del geometry['features'][index]
+
+        json.dump(geometry,file)
+        file.close()
